5f6e79816082dbd2b0b8a74a1b1170b6ade63f66
Hrithik sagar <hrithiksagar36@gmail.com>
2021-07-18 21:16:09 +0530

    git commands process

--- C++/8. Array/Leet Code/ThreeSum.cpp
+++ C++/8. Array/Leet Code/ThreeSum.cpp
@@ -23,4 +23,57 @@ int main()
     // yes it worked with its app
     // now tried after talking to Ritik!
     // done everyhtiung from first using git init cloninig form first
-    
+//    Last login: Sun Jul 18 20:48:18 on ttys001
+// hrithiksagar@Hrithiks-MacBook-Air Competetive-Programming % git init
+// Reinitialized existing Git repository in /Users/hrithiksagar/Documents/GitHub/Competetive-Programming/.git/
+// hrithiksagar@Hrithiks-MacBook-Air Competetive-Programming % git status
+// On branch main
+// Your branch is up to date with 'origin/main'.
+
+// Changes to be committed:
+//   (use "git restore --staged <file>..." to unstage)
+// 	modified:   C++/8. Array/Leet Code/ThreeSum.cpp
+
+// Changes not staged for commit:
+//   (use "git add <file>..." to update what will be committed)
+//   (use "git restore <file>..." to discard changes in working directory)
+// 	modified:   C++/8. Array/Leet Code/ThreeSum.cpp
+
+// hrithiksagar@Hrithiks-MacBook-Air Competetive-Programming % git add --all
+// hrithiksagar@Hrithiks-MacBook-Air Competetive-Programming % git status
+// On branch main
+// Your branch is up to date with 'origin/main'.
+
+// Changes to be committed:
+//   (use "git restore --staged <file>..." to unstage)
+// 	modified:   C++/8. Array/Leet Code/ThreeSum.cpp
+
+// hrithiksagar@Hrithiks-MacBook-Air Competetive-Programming % git commit -m "tried commiting using git commands by ritik"
+// [main f765efb] tried commiting using git commands by ritik
+//  1 file changed, 4 insertions(+), 1 deletion(-)
+// hrithiksagar@Hrithiks-MacBook-Air Competetive-Programming % git push
+// Enumerating objects: 11, done.
+// Counting objects: 100% (11/11), done.
+// Delta compression using up to 8 threads
+// Compressing objects: 100% (6/6), done.
+// Writing objects: 100% (6/6), 666 bytes | 666.00 KiB/s, done.
+// Total 6 (delta 4), reused 0 (delta 0), pack-reused 0
+// remote: Resolving deltas: 100% (4/4), completed with 4 local objects.
+// To https://github.com/hrithiksagar/Competetive-Programming.git
+//    fb9e029..f765efb  main -> main
+// hrithiksagar@Hrithiks-MacBook-Air Competetive-Programming % 
+
+//optional commands:
+// git clone url
+// git init
+// compulsory commands
+// git status
+// git add --all
+// git status
+// git commit -m "any message"
+// git push 
+// git log // it will show all previous commits
+// to change branch // code // 
+// default is default master branch
+// git checkout branchname
+
