9a9eb9b8008e1f447b59be2593b136955f1d56aa
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-07-24 12:31:39 +0530

    update level 1 9th question

---
+++ C++/8. Array/GeeksForGeeks/Level1_9.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+/**
+ * @brief Move all negative numbers to beginning and positive to end with constant extra space
+ * An array contains both positive and negative numbers in random order. Rearrange the array elements so that all negative numbers appear before all positive numbers.
+ * Examples : 
+ *Input:  -12, 11, -13, -5, 6, -7, 5, -3, -6
+ *Output: -12 -13 -5 -7 -3 -6 11 6 5
+ * 
+ */
+
+void rearrange(int arr[], int n){
+
+
+}
+void print(int arr[], int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout<< arr[i]<<" ";
+    }
+    cout<< endl;
+}
+
+
+int main(){
+    int arr[] = {-1,2,-3,4,-5,6,-7,8,-9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    rearrange(arr,n);
+    print(arr,n);
+    return 0;
+}
