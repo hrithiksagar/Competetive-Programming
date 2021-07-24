d048b7a5b3a057b8f94e418f6971d20c7f184ae0
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-07-24 13:18:40 +0530

    update level 1 9th question

--- C++/8. Array/GeeksForGeeks/Level1_9.cpp
+++ C++/8. Array/GeeksForGeeks/Level1_9.cpp
@@ -9,23 +9,35 @@ using namespace std;
  * 
  */
 
-void rearrange(int arr[], int n){
-
-
-}
-void print(int arr[], int n){
+void rearrange(int arr[], int n)
+{
+    int j = 0;
     for (int i = 0; i < n; i++)
     {
-        cout<< arr[i]<<" ";
+        if (arr[i] < 0)
+        {
+            if (i != j)
+            {
+                swap(arr[i], arr[j]);
+            }
+            j++;
+        }
     }
-    cout<< endl;
+}
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
-
-int main(){
-    int arr[] = {-1,2,-3,4,-5,6,-7,8,-9};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    rearrange(arr,n);
-    print(arr,n);
+int main()
+{
+    int arr[] = {-1, 2, -3, 4, -5, 6, -7, 8, -9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    rearrange(arr, n);
+    print(arr, n);
     return 0;
 }
