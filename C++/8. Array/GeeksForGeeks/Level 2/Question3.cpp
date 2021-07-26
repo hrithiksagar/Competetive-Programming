b11de1b1fec1a02c5ed0c1d0e7b96e09b0c3cd2b
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-07-26 21:57:08 +0530

    updated count pairs sum with hashmap concept

--- C++/8. Array/GeeksForGeeks/Level 2/Question3.cpp
+++ C++/8. Array/GeeksForGeeks/Level 2/Question3.cpp
@@ -81,10 +81,28 @@ we have
 
 
 */
-using namespace std::unordered_map;
+#include<unordered_map>
 int pairsumcount(int arr[], int n, int sum)
 {
-    unordered_map<int, int> m;
+    unordered_map<int, int> m; // m is the name of the map
+    // Storing all the content of all the elements in map m
+    for (int i = 0; i < n; i++)
+    {
+        m[arr[i]]++;
+    }
+    int twice_count = 0;
+    // iterating through each element and incrementing count (Every pair is counted twice)
+    for (int i = 0; i < n; i++)
+    {
+        twice_count += m[sum - arr[i]];
+        // if (arr[i], arr[i]) pair satisfies the condition, then we need to ensure taht count is decreased by one such that the (arr[i], arr[j]) pair is not considered
+        if(sum - arr[i] == arr[i])
+        {
+            twice_count--;
+        }
+    }
+    return twice_count/2;
+    
     
 }
 int main()
