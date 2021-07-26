2268fa329b20213672779b2c4bfed2be37d46b12
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-07-26 19:03:36 +0530

    added level 2 1st question

--- C++/8. Array/GeeksForGeeks/Level 2/question1.cpp
+++ C++/8. Array/GeeksForGeeks/Level 2/question1.cpp
@@ -12,29 +12,70 @@ brute force approach:
 #include<iostream>
 using namespace std;
 
-void rotate(int a[], int n){
-    int x = a[n-1];
-    int i;
-    for ( i = n - 1; i > 0; i--)
-    {
-        a[i] = a[i-1];
-    }
-    a[0] = x;
-}
+// void rotate(int a[], int n){
+//     int x = a[n-1];
+//     int i;
+//     for ( i = n - 1; i > 0; i--)
+//     {
+//         a[i] = a[i-1];
+//     }
+//     a[0] = x;
+// }
 
-int main(){
-    int a[] = {1,2,3,4,5,6};
-    int n = sizeof(a)/sizeof(a[0]);
-    cout<<"Given array is: ";
-    for (int i = 0; i < n; i++)
-    {
-        cout<<a[i]<<" ";
+// int main(){
+//     int a[] = {1,2,3,4,5,6};
+//     int n = sizeof(a)/sizeof(a[0]);
+//     cout<<"Given array is: ";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout<<a[i]<<" ";
+//     }
+//     rotate(a,n);
+//     cout<<"Rotated Array: ";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout<<a[i]<<" ";
+//     }
+//     return 0;
+// }
+/**
+ * @brief 
+ * ! Another Approach
+ * use 2 pointers i, j
+ * i = 0;
+ * j = n-1;
+ * until i != j:
+ *      a[i] , a[j] = a[j] , a[i]
+ *      i += 1;
+ * pass
+ * 
+ * 
+ */
+void rotate(int arr[], int n)
+{
+    int i = 0, j = n-1; // i and j pointing to first and last element respectively
+      while(i != j){
+      swap(arr[i], arr[j]);
+      i++;
     }
-    rotate(a,n);
-    cout<<"Rotated Array: ";
-    for (int i = 0; i < n; i++)
-    {
-        cout<<a[i]<<" ";
-    }
+}
+ 
+// Driver code
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5}, i;
+    int n = sizeof(arr) /
+            sizeof(arr[0]);
+ 
+    cout << "Given array is \n";
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+ 
+    rotate(arr, n);
+ 
+    cout << "\nRotated array is\n";
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+ 
     return 0;
 }
