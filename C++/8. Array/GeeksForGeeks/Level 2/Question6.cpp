c92fdc6bab11c9d33eb8f4aa04b2cb81cfbb6467
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-01 23:04:34 +0530

    Update

--- C++/8. Array/GeeksForGeeks/Level 2/Question6.cpp
+++ C++/8. Array/GeeksForGeeks/Level 2/Question6.cpp
@@ -1,5 +1,7 @@
 /**
  * @file Question6.cpp
  * Find common elements in three sorted arrays
+ * Given three arrays sorted in non-decreasing order, print all common elements in these arrays.
+ * Basically printing common elements
  * 
  */
