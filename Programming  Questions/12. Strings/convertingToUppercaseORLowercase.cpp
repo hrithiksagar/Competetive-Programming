cef4b4b3186db3a88a7255904b568f1f720f2704
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-05 23:49:39 +0530

    update strings

---
+++ Programming  Questions/12. Strings/convertingToUppercaseORLowercase.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{    
+    string str = "eJSBAJLCJZLFKWHNWKBdsabvkifhILCNEKLb";
+    cout<< 'a' - 'A'<<endl; // 32 : this is ascii a - ascii A
+    // to ocnvert fully uppercase
+
+
+    return 0;
+}
