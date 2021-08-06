5ba98303efd09c959f563024457a333c257b45a8
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-06 19:18:56 +0530

    update 1st string challenge

--- Programming  Questions/12. Strings/convertingToUppercaseORLowercase.cpp
+++ Programming  Questions/12. Strings/convertingToUppercaseORLowercase.cpp
@@ -6,8 +6,24 @@ int main()
 {    
     string str = "eJSBAJLCJZLFKWHNWKBdsabvkifhILCNEKLb";
     cout<< 'a' - 'A'<<endl; // 32 : this is ascii a - ascii A
-    // to ocnvert fully uppercase
-
-
+    // to Convert to  fully uppercase
+    for (int i = 0; i < str.size(); i++)
+    {
+        if(str[i] >= 'a' && str[i]<= 'z')
+        {
+            str[i] -= 32;
+        }
+    }
+    cout<< str<<endl;//EJSBAJLCJZLFKWHNWKBDSABVKIFHILCNEKLB
+    
+    // making them to lower case:
+   for (int i = 0; i < str.size(); i++)
+    {
+        if(str[i] >= 'A' && str[i]<= 'Z')
+        {
+            str[i] += 32;
+        }
+    } 
+    cout<< str<< endl; //ejsbajlcjzlfkwhnwkbdsabvkifhilcneklb
     return 0;
 }
