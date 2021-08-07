38ff8b04b9d12804ffa711ee04fed990564a3471
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-07 16:57:48 +0530

    update

---
+++ Programming  Questions/Bit Manipulation/Programs.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+
+int main(){
+/** get bit at perticular position
+ *n = 0101
+ * suppose we need to get bit at position, i = 2
+ * we use left shift for this 
+ * 1<<i = 0100
+ * then we do AND 
+ * 0101 & 0100 = 0100 : by this we get 
+ * then
+ * if n & (1<<i) != 0 then bit is 1
+ * 
+*/
+    return 0;
+}
