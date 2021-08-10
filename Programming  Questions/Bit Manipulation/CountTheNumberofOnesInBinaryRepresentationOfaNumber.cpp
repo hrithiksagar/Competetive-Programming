eaedad6c3ba25070d5b8e2decf7f10a8c085d627
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-10 22:48:25 +0530

    To count number of 1s in binary represemtation of a numner

---
+++ Programming  Questions/Bit Manipulation/CountTheNumberofOnesInBinaryRepresentationOfaNumber.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+/**
+ * @brief 
+ * n & n-1 = has same bits as n expect the rightmost set bit
+ * https://www.youtube.com/watch?v=s8l8NWII3qw&list=PLfqMhTWNBTe0b2nM6JHVCnAkhQRGiZMSJ&index=35&ab_channel=ApnaCollege
+ * n =19 = 01011
+ * n-1 = 18 = 01010
+ * n&n-1 = 01011 & 01010
+ *         01010 = 18
+ *         compare with 19
+ *         only rihgtmost 1 is the difference
+ *
+ *  n = 18 = 01010
+ * n-1 =17 = 01001
+ * n&n-1 = 18&17 = 01010 & 01001
+ *                 01001 =16
+ * n & n-1
+ */
+
+int main()
+{
+
+}
