3d23ac9caacaa5443ea6068c1cc45ec4817d9ce1
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-22 10:06:50 +0530

    update

--- Python/Chapter 1/using OS module.py
+++ Python/Chapter 1/using OS module.py
@@ -1,3 +1,17 @@
 # printing content of directory
 import os
 print(os.listdir())
+# import sys
+# def is_venv():
+#     return (hasattr(sys, 'real_prefix') or
+#             (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix))
+# if is_venv():
+#     print('inside ')
+# else:
+#     print('outside ')    
+# printing content of directory
+# import tensorflow
+# import numpy
+# print("Imported")
+
+
--- Python/Chapter 3 Strings/strings.py
+++ Python/Chapter 3 Strings/strings.py
@@ -2,4 +2,41 @@ a = 34          #int
 b = "Hrithik"   #string
 print(type(a))  #int
 print(type(b))  #string
+greeting = "Hello"
+name = "Hrithik"
+#concatinating 2 strings
+c = greeting+" " +name
+print(c)
+#       0123456
+print(name[0])
+#print(name[7])#IndexError: string index out of range
+# can gon upto 0-len(name)
+# cant change string
+#name[3] = "t" #TypeError: 'str' object does not support item assignment
 
+
+#slicing
+print(name[0:3])#Hri
+print(name[1:3])#ri
+print(name[:])#Hrithik
+print(name[:4])#Hrit same as name[0:4]
+#Negative indexes are:
+#-1 is the last character
+#-2 is the second last character
+#-3 is the third last character.....
+#-len(name) is the first character
+#-len(name)+1 is the second character
+#
+print(name[:-1])#hrithi
+print(name[-1])#k
+print(name[-1:5])#k
+          #my name in reverse order  
+         
+#3 parameteres
+print(name[1:4:1])#ri
+print(name[::-1])#kihtirh
+print(name[::2])#hihk
+print(name[::-2])#khih
+print(name[::3])#htk
+
+print()
