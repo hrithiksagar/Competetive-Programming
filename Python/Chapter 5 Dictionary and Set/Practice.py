5f3dfa461689b788aeeeba3aa71cbe477c057502
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-23 15:53:52 +0530

    Dict and sets practice qs 1

---
+++ Python/Chapter 5 Dictionary and Set/Practice.py
@@ -0,0 +1,12 @@
+"""
+1
+WAP to create a dict of hindi words with values of their eng translations provide user with an option to look it up!
+"""
+myDict = {
+    "panka": "Fan",
+    "Dabba": "Box",
+    "Vastu": "Item"
+}
+print("Options are: ", myDict.keys())
+a = input("enter hindi word: ")
+print("meaning of ur word is : ", myDict[a])
