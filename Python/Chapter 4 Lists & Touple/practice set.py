734eb432271f13b583d896095c18f62656c12cbb
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-23 14:25:23 +0530

    Practice Set

--- Python/Chapter 4 Lists & Touple/practice set.py
+++ Python/Chapter 4 Lists & Touple/practice set.py
@@ -1,4 +1,30 @@
-# 1
-# Q:
+# # 1
+# # Q: WAP to store seven fruits in list entered by user
+# lst = []
+# for i in range(7):
+#     lst.append(input("Enter fruit 1: "))
+#
+# print(lst)
+
+
+# # 2
+# # WAP to accept marks of 6 students and display them in sorted manner
+# lst = []
+# for i in range(6):
+#     lst.append(input("Enter marks: "))
+# lst.sort()
+# print(lst)
+#
+
+# # 3
+# # Wap to sum a list with 4 numbers
+# lst = [2,4,6,7]
+# print(lst[0]+lst[1]+lst[2]+lst[3])
+# print(sum(lst))
+
+# # 4
+# # find count of 0s in tuple
+# a=(7,0,8,0,0,9)
+# print(a.count(0)) # 3
 
 
