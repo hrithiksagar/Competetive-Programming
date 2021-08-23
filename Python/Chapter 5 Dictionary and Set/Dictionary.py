594f48617c16c26e613b835b80b9e358975aa8b8
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-23 14:34:10 +0530

    Dictionary

---
+++ Python/Chapter 5 Dictionary and Set/Dictionary.py
@@ -0,0 +1,18 @@
+# collection of key value pairs
+# syntax
+myDict = {
+    "fast": "In a quick manner",
+    "hrithik": "Learning to code",
+    "marks": [1, 2, 5],
+    "another Dictionary":
+        {
+            'key': 'this returns value of inside dictionary'
+        }
+}
+print(myDict['fast'])  # prints corresponding value to this key i.e  In a quick manner is returned
+print(myDict['hrithik'])  # Learning to code
+print(myDict['marks'])  # [1,2,5]
+print(myDict['another Dictionary']['key'])  # student # this is another dictionary key's "Value"
+# prints:
+# another Dictionary = Key of main dic
+# key = key of another dictionary
