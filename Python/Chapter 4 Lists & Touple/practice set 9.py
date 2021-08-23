6d510a751bc972e321cec2909434c15542b3d38b
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-23 19:15:58 +0530

    Dict and sets practice qs 9

---
+++ Python/Chapter 4 Lists & Touple/practice set 9.py
@@ -0,0 +1,9 @@
+"""
+Can u change the values inside a lost which is contained in set S
+S = [8,7,12,"hrithik",[1,2]}
+"""
+
+# s = {8,7,12,"hrithik",[1,2]} # cant storee list in set
+# so new set =
+s = {8, 7, 12, "hrithik", (1, 2)}
+print(s)
