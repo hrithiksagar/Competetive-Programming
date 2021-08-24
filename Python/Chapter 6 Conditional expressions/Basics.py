7a7a85a66854da4cd56855deb1022a97286dd118
Hrithik Sagar <hrithiksagar36@gmail.com>
2021-08-24 11:46:20 +0530

    Update Conditional Statements

---
+++ Python/Chapter 6 Conditional expressions/Basics.py
@@ -0,0 +1,48 @@
+"""
+else if
+if else
+if  elif else
+Syntax:
+if (Condition):
+    #statements
+elif(conditions):
+    #statements
+else:
+    #statements
+
+
+and = both the conditions that's joining and should be true
+or = if either one of the condition is true it works
+not = Inverts "true to false" and "false to true"
+"""
+
+# age = int(input("Enter your Age: "))
+# if(age>34 and age<56): # both the conditions needs to be true in AND condition
+#     print("You can work with us! ")
+# else:
+#     print("Sorry, you are not in our age limits!")
+
+
+# age = int(input("Enter your Age: "))
+# if(age>34 or age<56): # both the conditions needs to be true in AND condition
+#     print("You can work with us! ")
+# else:
+#     print("Sorry, you are not in our age limits!")
+
+"""
+IN & IS
+
+
+"""
+# is
+# a = None
+# if a is None:
+#     print(1)
+# else:
+#     print(0)
+
+
+# in
+a = [45, 50, 650]
+print(45 in a)  # True
+print(11 in a)   # False
---
+++ Python/Chapter 6 Conditional expressions/Practice set 1.py
@@ -0,0 +1,20 @@
+"""
+1. WAP to find greatest of four Numbers entered by user:
+
+2. WAp to find out weather a student is pass or fail, iof it requires total 40% and atleast 33%
+in each sub, to pass. Assume 3 Sub and take marks as an input from user
+
+3. A spam comment is defined as a text contanising following keywords:
+"Make a lot of money", "Buy now","Suscribe this", "Click this",
+WAP to detect these spams
+
+4. WAP to find wheater a given udername contains less than 10 characters or not
+
+5. WAP which finds out wheater a given name is present in a list or not
+
+
+
+"""
+
+#1 sol
+
